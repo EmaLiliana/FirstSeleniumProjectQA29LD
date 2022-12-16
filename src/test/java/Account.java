91571232cd06e259596a43911f6fa424cd81d67d
Liliana Dobre <lilianaemanueladobre@gmail.com>
2022-12-16 13:23:30 +0200

    CSS Selectors

--- src/test/java/Account.java
+++ src/test/java/Account.java
@@ -1,7 +1,10 @@
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Account {
@@ -19,15 +22,22 @@ public class Account {
         driver.findElement(By.cssSelector(".account-cart-wrapper span.label")).click();
         driver.findElement(By.cssSelector("div.links li.first a")).click();
         driver.findElement(By.cssSelector(".buttons-set span span")).click();
-        driver.findElement(By.cssSelector(".input-box #firstname")).click();
-        driver.findElement(By.cssSelector(".input-box #lastname")).click();
-        driver.findElement(By.cssSelector(".input-box #email_address")).sendKeys("ana@yahoo.com");
+        driver.findElement(By.cssSelector(".input-box #firstname")).sendKeys("leamara");
+        driver.findElement(By.cssSelector(".input-box #lastname")).sendKeys("Ana");
+        driver.findElement(By.cssSelector(".input-box #email_address")).sendKeys("leamara1234@yahoo.com");
         driver.findElement(By.cssSelector(".input-box #password")).sendKeys("Ana1234");
         driver.findElement(By.cssSelector(".input-box #confirmation")).sendKeys("Ana1234");
         driver.findElement(By.cssSelector(".control label")).click();
         driver.findElement(By.cssSelector(".buttons-set span span")).click();
         driver.findElement(By.cssSelector("li.current strong")).click();
 
+        WebElement changePasswordAccount = driver.findElement(By.cssSelector(".box-content a"));
+        Assert.assertTrue(changePasswordAccount.isDisplayed());
 
+
+    }
+    @After
+    public void quit(){
+        driver.quit();
     }
 }
--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -1,14 +1,11 @@
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.openqa.selenium.By.cssSelector;
-import static org.openqa.selenium.By.id;
 
 public class CheckoutTest {
     private WebDriver driver;
@@ -21,41 +18,33 @@ public class CheckoutTest {
         driver.get("http://testfasttrackit.info/selenium-test/");
     }
     @Test
-    public void checkoutProduct(){
+    public void checkoutProduct() throws InterruptedException {
         driver.findElement(cssSelector("li.nav-5 a")).click();
         driver.findElement(cssSelector(".actions a")).click();
         driver.findElement(cssSelector(".swatch-label img")).click();
         driver.findElement(cssSelector("#configurable_swatch_size span")).click();
         driver.findElement(cssSelector(".add-to-cart-buttons span span")).click();
-        driver.findElement(cssSelector("li span span")).click();
-        driver.findElement(cssSelector("li.control label")).click();
-        driver.findElement(cssSelector(".buttons-set span span")).click();
-        driver.findElement(id("billing:firstname")).sendKeys("Pop");
-        driver.findElement(By.cssSelector("ul [title=\"Last Name\"]")).sendKeys("Ana");
-        driver.findElement(By.cssSelector("[title=\"Email Address\"]")).sendKeys("ana@yahoo.com");
-        driver.findElement(By.cssSelector("li.wide input")).sendKeys("Pascu Street");
+        driver.findElement(cssSelector(".cart-totals [type=\"button\"]")).click();
+        driver.findElement(By.cssSelector("#onepage-guest-register-button > span > span")).click();
+        driver.findElement(By.cssSelector("#billing\\:firstname")).sendKeys("Dma");
+        driver.findElement(By.cssSelector("#billing\\:lastname")).sendKeys("Ana");
+        driver.findElement(By.cssSelector("#billing\\:street1")).sendKeys("Pascu");
+        driver.findElement(By.cssSelector("#billing\\:email")).sendKeys("pope@yahoo.com");
+        driver.findElement(By.cssSelector("#billing\\:street1")).sendKeys("Pascu");
         driver.findElement(By.cssSelector("#billing\\:city")).sendKeys("Cluj");
         driver.findElement(By.cssSelector(".validate-select.required-entry option[value=\"1\"]")).click();
         driver.findElement(By.cssSelector("#billing\\:postcode")).sendKeys("123456");
         driver.findElement(By.cssSelector("#billing\\:country_id > option:nth-child(212)")).click();
-        driver.findElement(By.cssSelector("#billing\\:telephone")).sendKeys("0000884596");
+        driver.findElement(By.cssSelector("#billing\\:telephone")).sendKeys("0008845969");
+        driver.findElement(By.cssSelector("#billing\\:use_for_shipping_yes")).click();
         driver.findElement(By.cssSelector("#billing-buttons-container span span")).click();
-  //      driver.findElement(By.cssSelector("#shipping-method-buttons-container span span")).click();
-  //      driver.findElement(By.cssSelector("#payment-buttons-container span span")).click();
-  //      driver.findElement(By.cssSelector(".btn-checkout span span")).click();
 
+        driver.findElement(By.cssSelector("#shipping-method-buttons-container > button > span > span")).click();
 
-  //      WebElement grandTotal= driver.findElement(cssSelector("strong span.price"));
-  //      Assert.assertTrue(grandTotal.isDisplayed());
-
-  //      WebElement checkoutSuccsessMessage = driver.findElement(cssSelector(".col-main h1"));
-  //      String expectedText = "YOUR ORDER HAS BEEN RECEIVED.";
-  //      String actualText = checkoutSuccsessMessage.getText();
-   //     Assert.assertEquals(actualText,expectedText);
-
-
-        WebElement requiredFields= driver.findElement(cssSelector("#co-billing-form p"));
-        Assert.assertTrue(requiredFields.isDisplayed());
+        Thread.sleep(100);
+        driver.findElement(By.cssSelector("#payment-buttons-container > button > span > span")).click();
+        Thread.sleep(100);
+        driver.findElement(By.cssSelector("#checkout-review-submit span span")).click();
 
 
     }
